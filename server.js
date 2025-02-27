fc07340d40e573310a5e9c76344fd20ac9c95e11
Matrixq35 <toomatris@gmail.com>
2025-02-27 21:14:22 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 const cors = require("cors");
+const path = require('path');
+
 
 const app = express();
 const PORT = 8080;
