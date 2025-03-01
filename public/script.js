5f7b81a7822142b8327039cb0af5a62e5e97cc81
Matrixq35 <toomatris@gmail.com>
2025-03-01 22:34:33 +0500

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", async () => {
     try {
         window.Telegram.WebApp.ready();
+        window.Telegram.WebApp.expand();
 
         // Получаем данные пользователя из Telegram WebApp API
         const userData = window.Telegram.WebApp.initDataUnsafe;
@@ -23,6 +24,34 @@ document.addEventListener("DOMContentLoaded", async () => {
             throw new Error("Ошибка при получении данных от сервера");
         }
 
+         // Переключение вкладок
+    const buttons = document.querySelectorAll(".nav-btn");
+    const sections = document.querySelectorAll(".tab-content");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            const target = button.getAttribute("data-target");
+
+            // Убираем активный класс у всех кнопок
+            buttons.forEach(btn => btn.classList.remove("active"));
+
+            // Добавляем активный класс нажатой кнопке
+            button.classList.add("active");
+            console.log('кнопка нажата');
+            
+            // Скрываем все секции
+            sections.forEach(section => section.classList.remove("active"));
+
+            // Показываем нужную секцию
+            const targetSection = document.getElementById(target);
+            if (targetSection) {
+                targetSection.classList.add("active");
+            } else {
+                console.warn(`Секция с ID ${target} не найдена!`);
+            }
+        });
+    });
+
         const userDataFromServer = await response.json();
 
         // Вставляем данные в HTML
@@ -30,11 +59,12 @@ document.addEventListener("DOMContentLoaded", async () => {
         if (userInfoDiv) {
             userInfoDiv.innerHTML = `
                 <p><strong>Ваш ID:</strong> ${userDataFromServer.telegram_id}</p>
-                <p><strong>Баланс:</strong> ${userDataFromServer.balance} ₽</p>
+                <p><strong>Баланс:</strong> ${userDataFromServer.balance} VITS</p>
             `;
         }
     } catch (error) {
         console.error("Ошибка:", error.message);
         document.getElementById("userInfo").textContent = "Ошибка загрузки данных";
     }
+
 });
